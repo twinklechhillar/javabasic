e8f01230b330aafbf24f9369e14ffa5da01a2e7a
twinklechhillar <twinklechhillar89@gmail.com>
2026-05-17 16:15:59 +0530

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        String name ="twinkle";
+        int age = 20;
+        double cgpa = 8.5; 
+        
+        System.out.println("Name:" +name);
+        System.out.println("Age:"+age);
+        System.out.println("Cgpa:"+cgpa);
+    }
+}
